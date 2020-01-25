ab958d2f2211505b07a1aa438db14b28aa90bb9a
joshdellaz <josh.lazaruk@gmail.com>
2020-01-25 02:17:13 -0700

    Added some spi calls and lots of comments

--- uhf_link_layer_prototype/uhf.h
+++ uhf_link_layer_prototype/uhf.h
@@ -11,16 +11,19 @@
 typedef struct {
   uint16_t preamble;
   uint32_t sync_word;
+  //add sequence numbers?
   uint8_t data_length;
   union __attribute__((__packed__)){
     uint8_t* data;
     uint16_t* data16;
     uint32_t* data32;
   };
+  //add crc
   
 } uhf_frame_t;
 
 int uhf_init();
 int uhf_send(uint8_t* data);
+int uhf_receive(uint8_t* data);
 
 #endif
--- uhf_link_layer_prototype/uhf/uhf.c
+++ uhf_link_layer_prototype/uhf/uhf.c
@@ -1,9 +1,17 @@
 #include <csp/csp.h>
+#include "spi.h"
 
 //dummy values for now
 #define PREAMBLE_2FSK 1
 #define PREAMBLE_3FSK 2
 #define PREAMBLE_4FSK 3
+#define SPI_NUMBER spiREG3
+
+spiDAT1_t dataconfig1_t;
+dataconfig1_t.CS_HOLD = FALSE;
+dataconfig1_t.WDEL    = FALSE;
+dataconfig1_t.DFSEL   = SPI_FMT_0;
+dataconfig1_t.CSNR    = 0x00;
 
 
 //Not yet working, need to convert frame into array
@@ -36,13 +44,44 @@ uint32_t get_crc32(uhf_frame_t* frame) {
    return ~crc;
 }
 
-int uhf_send(uint8_t* data){
-  uhf_frame_t frame;
-  frame.data_length = sizeof(data);
-  frame.preamble = PREAMBLE_2FSK;
-  frame.sync_word = 16; //unique consistent value needs to be chosen for sync word
-  frame.data = data; //MTU to be enforced at interface level, or here
-  uint32_t crc = get_crc32(frame);
+int uhf_init(){
 
-  //spi send to ADF here
+      //Should this include the init for the chip itself?
+
+}
+
+int uhf_send(uint8_t* data){
+
+   /* Frame-ize the data */ //Maybe worth having seperate functions for a) framing large data buffers that need more than 1 frame,
+                            //b) Sending multiple consecutive frames over SPI
+   uhf_frame_t frame;
+   frame.data_length = sizeof(data)/2;//added /2 to make length multiple of 16 bits
+   frame.preamble = PREAMBLE_2FSK;
+   frame.sync_word = 16; //unique consistent value needs to be chosen for sync word
+   frame.data = data; //MTU to be enforced at interface level, or here
+   uint32_t crc = get_crc32(frame);
+
+
+   //Should FEC be done here? or before data is passed to uhf_send.
+   //Does just data get FEC'd, or do other parts of frame too?
+
+
+   //add chip select gpio signal
+   spiTransmit(SPI_NUMBER, &dataconfig1_t, 1, frame->preamble);
+   spiTransmit(SPI_NUMBER, &dataconfig1_t, 2, frame->sync_word);
+   spiTransmit(SPI_NUMBER, &dataconfig1_t, frame->data_length, frame->data);//each block is 16 bits
+   //what happens if data isn't a 16 bit multiple?
+   //Add return 1 for error
+
+   return 0;
+}
+
+
+int uhf_receive(uint8_t* data){
+
+   //MCU is only notified when sync word is detected
+   //spi only receives bits after sync word
+
+
+
 }
